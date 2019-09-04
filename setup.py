27dc0b787aef1b6e6c2497817f69b6710b66d45c
oskar_deepbi <oskar@deep.bi>
2019-09-04 12:04:16 +0200

    More information in setup.py and .travis.yml

--- setup.py
+++ setup.py
@@ -7,8 +7,17 @@ MAINTAINER_EMAIL = 'oskar.jarczyk@gmail.com'
 LICENSE = 'CC BY-SA 3.0'
 URL = 'https://github.com/oskar-j/fastcat'
 VERSION = '0.1.2'
-KEYWORDS = ['wikipedia', 'categories', 'wiki-api', 'knowledge engineering']
-CLASSIFIERS = ['Development Status :: 3 - Alpha', ]
+DOWNLOAD_URL = 'https://github.com/oskar-j/fastcat/archive/v_01_2.tar.gz'
+KEYWORDS = ['Wikipedia', 'categories', 'wiki-api', 'knowledge engineering']
+CLASSIFIERS = ['Development Status :: 3 - Alpha',
+               'Intended Audience :: Science/Research',
+               'Topic :: Scientific/Engineering :: Artificial Intelligence',
+               'License :: OSI Approved',
+               'Programming Language :: Python :: 3',
+               'Programming Language :: Python :: 3.5',
+               'Programming Language :: Python :: 3.6',
+               'Programming Language :: Python :: 3.7',
+               ]
 
 
 def setup_package():
@@ -25,6 +34,7 @@ def setup_package():
         maintainer_email=MAINTAINER_EMAIL,
         license=LICENSE,
         url=URL,
+        download_url=DOWNLOAD_URL,
         packages=find_packages(exclude=['*tests*']),
         install_requires=['redis', 'pycountry'])
 
